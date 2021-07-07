d61e57c8deff54ea1a33fe75eee59d3aa337545c
peter <perter@126.com>
2021-07-07 17:58:15 +0800

    init

--- src/pages/admin/index.vue
+++ src/pages/admin/index.vue
@@ -5,7 +5,7 @@
     >
         <el-row class="padding-md">
             <el-col
-                    :span="6"
+                    :span="12"
                     class="margin-lg-bottom"
             >
                 <div
@@ -24,26 +24,7 @@
                 </div>
             </el-col>
             <el-col
-                    :span="6"
-                    class="margin-lg-bottom"
-            >
-                <div
-                        class="text-center"
-                        style="margin: auto;width: 90px;"
-                >
-                    <el-button
-                            type="default"
-                            class="margin-sm-bottom"
-                            style="width: 80px;height: 80px;"
-                            circle
-                    >
-                        {{ newBooks }}
-                    </el-button>
-                    <span>新增小说</span>
-                </div>
-            </el-col>
-            <el-col
-                    :span="6"
+                    :span="12"
                     class="margin-lg-bottom"
             >
                 <div
@@ -61,35 +42,8 @@
                     <span>新增文章</span>
                 </div>
             </el-col>
-            <el-col
-                    :span="6"
-                    class="margin-lg-bottom"
-            >
-                <div
-                        class="text-center"
-                        style="margin: auto;width: 90px;"
-                >
-                    <el-button
-                            type="default"
-                            class="margin-sm-bottom"
-                            style="width: 80px;height: 80px;"
-                            circle
-                    >
-                        {{ newVideo }}
-                    </el-button>
-                    <span>新增视频</span>
-                </div>
-            </el-col>
             <el-col :span="24" style="height: 240px;">
                     <e-charts :option="options"/>
-                    <i
-                            v-if="requestSiteUpdateData"
-                            class="el-icon-loading "
-                            style="font-size: 36px;"
-                    />
-            </el-col>
-            <el-col :span="24" style="height: 240px;">
-                    <e-charts :option="video_options"/>
                     <i
                             v-if="requestSiteUpdateData"
                             class="el-icon-loading "
@@ -138,42 +92,9 @@
                 newBooks: 0,
                 newArticle: 0,
                 newVideo: 0,
-                video_options: {
-                    title: {
-                        text: '视频更新统计'
-                    },
-                    tooltip: {show: true},
-                    xAxis: {
-                        axisPointer: {
-                            snap: true,
-                            label: {
-                                show: true,
-                                formatter: function (params) {
-                                    return params.value
-                                }
-                            },
-                            handle: {
-                                show: false
-                            }
-                        },
-                        type: 'category',
-                        data: []
-                    },
-                    yAxis: {
-                        type: 'value'
-                    },
-                    series: [{
-                        data: [],
-                        type: 'line',
-                        areaStyle: {}
-                    }, {
-                        data: [],
-                        type: 'line'
-                    }]
-                },
                 options: {
                     title: {
-                        text: '章节更新统计'
+                        text: '30日新增用户'
                     },
                     tooltip: {show: true},
                     xAxis: {
@@ -197,7 +118,7 @@
                         type: 'value'
                     },
                     series: [{
-                        data: [1, 2, 3, 4, 5, 6],
+                        data: [],
                         type: 'line',
                         areaStyle: {}
                     }]
@@ -223,7 +144,6 @@
                     that.siteUpdateData = data;
                     that.requestSiteUpdateData = false;
                     that.drawChapters(data);
-                    that.drawVideos(data);
                     that.setYesterdayValue(data);
                 }, function (err) {
                     console.debug("统计数据请求失败", err);
@@ -236,31 +156,8 @@
                 for (var k = 0; k < data.length; k++) {
                     if (data[k].a_date === yesterday) {
                         this.newUsers = data[k].new_users
-                        this.newBooks = data[k].new_book
                         this.newArticle = data[k].new_articles
-                        this.newVideo = data[k].new_videos
                         break
-                    }
-                }
-            },
-
-            drawVideos(data) {
-                let showDays = 30;
-                let now = (new Date()).getTime() - showDays * 24 * 3600 * 1000;
-                for (let j = 0; j < showDays; j++) {
-                    this.video_options.xAxis.data.push(new Date(now).format('MM-dd'));
-                    now = now + 24 * 3600 * 1000;
-                    this.video_options.series[0].data.push(0);
-                }
-
-                for (let i = 0; i < data.length; i++) {
-                    let tempDate = (new Date()).getTime() - showDays * 24 * 3600 * 1000
-                    for (let k = 0; k < showDays; k++) {
-                        if (data[i].a_date.toString() === (new Date(tempDate).format('yyyyMMdd'))) {
-                            this.video_options.series[0].data[k] = (data[i].new_video_source)
-                            break
-                        }
-                        tempDate += 24 * 3600 * 1000
                     }
                 }
             },
@@ -270,20 +167,19 @@
                 for (let j = 0; j < showDays; j++) {
                     this.options.xAxis.data.push(new Date(now).format('MM-dd'));
                     now = now + 24 * 3600 * 1000;
-                    this.options.series[0].data.push(0)
+                    this.options.series[0].data[j] = 0;
                 }
 
                 for (let i = 0; i < data.length; i++) {
                     let tempDate = (new Date()).getTime() - showDays * 24 * 3600 * 1000;
                     for (let k = 0; k < showDays; k++) {
                         if (data[i].a_date.toString() === (new Date(tempDate).format('yyyyMMdd'))) {
-                            this.options.series[0].data[k] = data[i].new_chapters;
+                            this.options.series[0].data[k] = data[i].new_users;
                             break;
                         }
                         tempDate += 24 * 3600 * 1000
                     }
                 }
-                console.debug(this.options.series[0])
             }
         }
     });
