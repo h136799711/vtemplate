fef129852481a13cd005cf3b2b8af1d9dc4cffb0
peter <perter@126.com>
2021-07-07 17:49:34 +0800

    init

--- src/api/staticsApi.js
+++ src/api/staticsApi.js
@@ -6,6 +6,6 @@ const staticsApi = {
 	query(data, suc, fail) {
 		defaultPost ('/100/Statics/index', Object.assign({lc_key: '100_statics_index', lc_expire: 8 * 3600}, data), suc, fail)
 	}
-}
+};
 
 export default staticsApi;
